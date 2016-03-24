45fb79f2abd91420685001348c9ff09ee2d1312e
PiratePowWow <Thi3v3ry>
2016-03-24 09:21:29 -0400

    8-3 Complete

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,33 +1,62 @@
 (ns purchases-clojure.core 
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [ring.middleware.params :as p]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main []
-  (println "Enter a Category: Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry")
-  (println "Or Type q to Exit")
-  (loop []
-        (let [  category (read-line)
-                purchases (slurp "purchases.csv")
+(def purchases
+        (let [  purchases (slurp "purchases.csv")
                 purchases (str/split-lines purchases)
                 purchases (map (fn [line]
                                  (str/split line #","))
                             purchases)
                 header (first purchases)
-                purchases (rest purchases)
                 purchases (map (fn [line]
                                  (apply hash-map (interleave header line)))
                              purchases)
-                purchases (walk/keywordize-keys purchases)
-                purchases (filter (fn [line]
-                                    (= (:category line) category))
-                            purchases)]
-          (when (not= category "q")
-              (println purchases)
-              (spit "filtered_purchases.edn" (pr-str purchases))
-              (println "Enter a Category: Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry")
-              (println "Or Type q to Exit")
-              (recur)))))
+                purchases (walk/keywordize-keys purchases)]
+          purchases))
+
+(def header (vals (first purchases)))
+
+(def categories (vec (set (map (fn [purchase]
+                                (:category purchase))
+                            (rest purchases)))))
+
+(defn filtered-purchases [category] (filter (fn [line]
+                                              (= (:category line) category))
+                                      (rest purchases)))
+
+(defn categories-html []
+  [:div
+    (map (fn [category]
+            [:span
+              [:a {:href (str "/?category=" category)} category]
+              " "])
+      categories)])
+
+(defn purchases-html [purchases]
+  [:ol
+    (map (fn [purchase]
+            [:li (str/join " " (vals purchase))])
+        (rest purchases))])
+
+(c/defroutes app
+  (c/GET "/" request
+    (let [params (:params request) 
+          category (get params "category") 
+          category (or category "Alcohol")
+          purchases (filtered-purchases category)]
+        (h/html [:html
+                  [:body
+                    (categories-html)
+                    (purchases-html (rest purchases))]]))))
+                    
+(defn -main []
+  (j/run-jetty (p/wrap-params app) {:port 3000}))  
       
       
       
